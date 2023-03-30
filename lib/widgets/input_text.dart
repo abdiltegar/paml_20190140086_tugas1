c809c5f95bf57b2a6e279576a4d55cfa3549a216
unknown <abdiltegar12@gmail.com>
2023-03-30 10:41:52 +0700

    welcome page

---
+++ lib/widgets/input_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class InputText extends StatefulWidget {
+  const InputText({super.key});
+
+  @override
+  State<InputText> createState() => _InputTextState();
+}
+
+class _InputTextState extends State<InputText> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
